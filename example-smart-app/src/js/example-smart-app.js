131d01223cd1312ff350d8db421fc6e72b9132f4
tlomaro <46379190+tlomaro@users.noreply.github.com>
2019-01-07 16:06:03 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -42,7 +42,8 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var stage = byCodes('75620-5');
+          var cstage = byCodes('75620-5');
+          var pstage = byCodes('75621-3');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -61,7 +62,8 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-          p.stage = getQuantityValueAndUnit(stage[0]);
+          p.cstage = getQuantityValueAndUnit(cstage[0]);
+          p.pstage = getQuantityValueAndUnit(pstage[0]);
 
           ret.resolve(p);
         });
@@ -86,7 +88,8 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      stage: {value: ''},
+      cstage: {value: ''},
+      pstage: {value: ''},
     };
   }
 
@@ -130,7 +133,8 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    $('#stage').html(p.stage);
+    $('#cstage').html(p.cstage);
+    $('#pstage').html(p.pstage);
   };
 
 })(window);
